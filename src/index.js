a58076b5f16856d2cc9a421f1b71bf49f043af03
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-12-12 14:10:28 +0800

    改成多任务

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-var images = [];
 var imageSize = '_200x200'
-var queryKey = '毛衣女'
-var dir = 'data/' + queryKey + '/'
-
-var casper = require('casper').create({
-    pageSettings: {
-        loadImages: false
-    }
-});
+var queryKey = ['衬衫男', '衬衫女', '毛衣男', '毛衣女']
 
 function getImages() {
     var images = document.querySelectorAll('#mainsrp-itemlist img');
@@ -28,39 +20,53 @@ Array.prototype.unique = function () {
     return res;
 }
 
-casper.start('https://www.taobao.com/', function () {
-    this.waitForSelector('form[action="//s.taobao.com/search"]');
-});
+queryKey.forEach(function(keyword) {
+    console.log(keyword)
+    var casper = require('casper').create({
+        pageSettings: {
+            loadImages: false
+        }
+    });
+    process(casper, keyword)
+})
 
-casper.then(function () {
-    this.fill('form[action="//s.taobao.com/search"]', { q: queryKey }, true);
-});
-
-casper.wait(2000)
-
-// 收集图片地址
-for (var j = 0; j < 100; j++) {
+function process(casper, keyword) {
+    var images = []
+    casper.start('https://www.taobao.com/', function () {
+        this.waitForSelector('form[action="//s.taobao.com/search"]');
+    });
+    
     casper.then(function () {
-        this.echo('正在爬取...')
-        if (j > 0) this.clickLabel('下一页', 'span')
-        this.wait(1000, function () {
-            this.scrollToBottom()
-            images = images.concat(this.evaluate(getImages));
-        })
+        this.fill('form[action="//s.taobao.com/search"]', { q: keyword }, true);
+    });
+    
+    casper.wait(2000)
+    
+    // 收集图片地址
+    for (var j = 0; j < 1; j++) {
+        casper.then(function () {
+            this.echo('正在爬取...')
+            if (j > 0) this.clickLabel('下一页', 'span')
+            this.wait(1000, function () {
+                this.scrollToBottom()
+                images = images.concat(this.evaluate(getImages));
+            })
+        });
+    }
+    
+    // 下载图片
+    casper.then(function () {
+        images = images.unique()
+        for (var i = 0; i < images.length; i++) {
+            if (images[i]) {
+                this.echo('正在下载 ' + keyword + i)
+                this.download('http:' + images[i] + imageSize + '.jpg', 'data/' + keyword + '/' + i + '.jpg');
+            }
+        }
+    });
+    
+    casper.run(function () {
+        this.echo('下载完成').exit()
     });
 }
 
-// 下载图片
-casper.then(function () {
-    images = images.unique()
-    for (var i = 0; i < images.length; i++) {
-        if (images[i]) {
-            this.echo('正在下载 ' + i)
-            this.download('http:' + images[i] + imageSize + '.jpg', dir + i + '.jpg');
-        }
-    }
-});
-
-casper.run(function () {
-    this.echo('下载完成').exit()
-});
